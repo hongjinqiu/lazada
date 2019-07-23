615cdb78f554e7091d1846b56bf9602290f4ad25
jqhong <jqhong@onlyou.com>
2019-07-23 18:12:32 +0800

    mysql 测试

--- src/main/java/com/github/hongjinqiu/App.java
+++ src/main/java/com/github/hongjinqiu/App.java
@@ -7,13 +7,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 @EnableScheduling
 @SpringBootApplication
 @EnableConfigurationProperties({YmlConfig.class})
 public class App implements CommandLineRunner {
 
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+    public static void main(String[] args) throws Exception {
+//        SpringApplication.run(App.class, args);
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/lazada?useUnicode=true&characterEncoding=UTF8", "root", "3564764");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from sys_print_template");
+            while (rs.next()) {
+                String templateName = rs.getString("template_name");
+                System.out.println("templateName is:" + templateName);
+            }
+        }
     }
 
     @Override
